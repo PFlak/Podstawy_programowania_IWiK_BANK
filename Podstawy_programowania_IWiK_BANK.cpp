5f5f10cd8980fa48f677ff043942ea3a639d8a45
PiotrF <piotrf@aiseemo.pl>
2023-03-19 09:36:11 +0100

    Add project files.

---
+++ Podstawy_programowania_IWiK_BANK.cpp
@@ -0,0 +1,12 @@
+﻿// Podstawy_programowania_IWiK_BANK.cpp : Defines the entry point for the application.
+//
+
+#include "Podstawy_programowania_IWiK_BANK.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
