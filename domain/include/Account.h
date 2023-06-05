db6355f660974c73ca8f0e0af191fdda55b91c3d
Trejden <michamel08@gmail.com>
2023-06-05 22:39:54 +0200

    changed domain models from classes into structs

--- domain/include/Account.h
+++ domain/include/Account.h
@@ -1,63 +1,13 @@
-#using "Currency.h"
+#pragma once
 
-public class Account
+using namespace std;
+struct Account
 {
-	public class Currency
-	{
-	private:
-		string name;
-		float value;
-	public:
-		string GetName()
-		{
-			return this->name;
-		}
-		float GetValue()
-		{
-			return this->value;
-		}
-	};
-private:
 	int id;
 	int accountNumber;
 	int userId;
-	Currency currency;
+	string currency;
 	int balance;
 	string type;
 	int interestRate;
-public:
-	int GetId()
-	{
-		return this->id;
-	}
-
-	int GetAccountNumber()
-	{
-		return this->accountNumber;
-	}
-
-	int GetUserId()
-	{
-		return this->userId;
-	}
-
-	Currency GetCurrency()
-	{
-		return this->currency;
-	}
-
-	int GetBalance()
-	{
-		return this->balance;
-	}
-
-	string GetType()
-	{
-		return this->type;
-	}
-
-	int GetInterestRate()
-	{
-		return this->InterestRate();
-	}
 };
--- domain/include/User.h
+++ domain/include/User.h
@@ -1,10 +1,9 @@
 #PRAGMA ONCE
-#include <cstdlib>
 
+using namespace std;
 
-public class User
+struct User
 {
-private:
 	int id;
 	string login;
 	string password;
@@ -13,63 +12,5 @@ private:
 	string personalCode;
 	string mail;
 	string phoneNumber;
-public:
-	User(int id,
-		string login,
-		string password,
-		string name,
-		string surname,
-		string personalCode,
-		string mail,
-		string phoneNumber)
-	{
-		this->id = id;
-		this->login = login;
-		this->password = password;
-		this->name = name;
-		this->surname = surname;
-		this->personalCode = personalCode;
-		this->mail = mail;
-		this->phoneNumber = phoneNumber;
-	}
-
-	int GetId() 
-	{
-		return this->id;
-	}
-
-	string GetLogin() 
-	{
-		return this->login;
-	}
-
-	string GetName()
-	{
-		return this->name;
-	}
-
-	string GetSurname()
-	{
-		return this->surname;
-	}
-
-	string GetPersonalCode()
-	{
-		return this->personalCode;
-	}
-
-	string GetMail()
-	{
-		return this->mail;
-	}
-
-	string GetPhoneNumber()
-	{
-		return this->phoneNumber;
-	}
-
-	bool Login(string password)
-	{
-		return password == this->password;
-	}
+	bool isEmployee;
 };
