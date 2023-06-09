311140a0bc57965e8757f1d8cac46f1bd4f76f57
Drijgon1 <krzysztof.broniak@student.pk.edu.pl>
2023-06-09 12:48:42 +0200

    history component

--- GMDB_frontend/src/home/components/history/history/history.component.ts
+++ GMDB_frontend/src/home/components/history/history/history.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ApiServiceService } from 'src/apiService/api-service.service';
+import { History } from 'src/models/History';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  styleUrls: ['./history.component.css']
+  styleUrls: ['./history.component.css'],
 })
 export class HistoryComponent {
+  public history: History[] = [];
 
+  constructor(api: ApiServiceService) {
+    api.TransactionHistory$.subscribe((response) => {
+      this.history = response;
+    });
+  }
 }
